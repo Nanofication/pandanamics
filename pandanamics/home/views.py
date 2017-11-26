f2e88129d619c761238a05ec90943ad1ac84e22d
Nanofication <nansquared@gmail.com>
2017-11-25 23:56:02 -0500

    Created tutorial files from Django tutorial

--- pandanamics/home/views.py
+++ pandanamics/home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/home.html') # location of html template
+
+def contact(request):
+    return render(request, 'home/basic.html',
+                  {'content':['If you would like to contact me, please email me',
+                              'emailplaceholder@gmail.com']})
