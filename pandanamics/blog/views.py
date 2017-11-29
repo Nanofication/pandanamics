30841a5a32416ddb1a0a4664c0153fb5b79b2844
Nanofication <nansquared@gmail.com>
2017-11-29 00:02:31 -0500

    Carousel displays but does not show actual image

--- pandanamics/blog/views.py
+++ pandanamics/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Topic
+
+def carousel(request):
+    template = loader.get_template('home/carousel.html')
+    context = {
+        'topics' : Topic.objects.all()
+    }
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
--- pandanamics/home/views.py
+++ pandanamics/home/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
 
 # Create your views here.
 
